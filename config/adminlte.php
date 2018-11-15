e8e06dd6cb0dc638a3b5660814e454c75dcbc780
GuilhermSDias <guilhermedude.7@gmail.com>
2018-11-15 11:49:55 -0300

    Renomeando Pasta pt-BR da Resource para pt-br

--- config/adminlte.php
+++ config/adminlte.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 2',
+    'title' => 'Demandaí',
 
     'title_prefix' => '',
 
@@ -30,9 +30,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Demandaí</b>IFTO',
 
-    'logo_mini' => '<b>A</b>LT',
+    'logo_mini' => '<b>Dm</b>Aí',
 
     /*
     |--------------------------------------------------------------------------
