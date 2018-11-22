c014d0b8345a883401cf847a1ac6798541d1bf88
GuilhermSDias <guilhermedude.7@gmail.com>
2018-11-22 01:11:32 -0300

    Adição das telas de listagem de usuários (do tipo PROPI e PESQUISADOR)

---
+++ resources/views/vendor/adminlte/users/listar-propi.blade.php
@@ -0,0 +1,51 @@
+@extends('adminlte::page')
+
+@section('title', 'Demandaí')
+
+@section('content_header')
+    <section class="content-header">
+        <h1>
+            Lista de Usuários
+        </h1>
+    </section>
+@stop
+
+@section('content')
+            <section class="content">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="box">
+                            <div class="box-header">
+                                <h3 class="box-title">Usuários do Tipo PROPI</h3>
+                            </div>
+                            <div class="box-body">
+                                <table id="usuarios-propi" class="table table-bordered table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @forelse($users as $user)
+                                    <tr>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                    </tr>
+                                    @empty
+                                        Não há usuários
+                                    @endforelse
+                                    </tbody>
+                                    <tfoot>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                    </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+@stop
