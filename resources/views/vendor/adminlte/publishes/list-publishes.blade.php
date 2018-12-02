c066054e8696b991d43bea843af3dd6ee717bedc
gerverson <gerversonsa@gmail.com>
2018-12-02 18:10:53 -0300

    correção de detalhes

--- resources/views/vendor/adminlte/publishes/list-publishes.blade.php
+++ resources/views/vendor/adminlte/publishes/list-publishes.blade.php
@@ -1,7 +1,5 @@
 @extends('adminlte::page')
-
 @section('title', 'Demandaí')
-
 @section('content_header')
     <section class="content-header">
         <h1>
@@ -9,8 +7,8 @@
         </h1>
     </section>
 @stop
-
 @section('content')
+
     <section class="content">
         <div class="row">
             <div class="col-xs-12">
@@ -43,34 +41,27 @@
                             <h3 class="title" style="color: green">Editais Abertos</h3>
                             <tbody>
                             @forelse($publishes as $publish)
-
                                 <a class="card" href="{{url('detalhe-edital/show',$publish->id)}}">
                                     @if($publish->date_closure >=  Carbon\Carbon::today())
                                     <span><h3>{{$publish->title}}</h3></span>
                                     <p class="text" style="color: black">{{$publish->description}}</p>
-                                    <span class="text" style="color: black">{{date('d/m/Y', strtotime($publish->date_closure))}}</span>
+                                    <span class="text" style="color: black">Encerramento de Inscrições: {{date('d/m/Y', strtotime($publish->date_closure))}}</span>
                                     @endif
                                 </a>
-
                             @empty
                                 <b>Não há editais cadastrados até o momento!</b>
                             @endforelse
                             </tbody>
-
                             <h3 class="title" style="color: red">Editais Fechados</h3>
                             <tbody>
                             @forelse($publishes as $publish)
-
                                 <a class="card" href="{{url('detalhe-edital/show',$publish->id)}}">
-
                                     @if($publish->date_closure < Carbon\Carbon::today())
                                     <span><h3>{{$publish->title}}</h3></span>
-                                    <p class="text" style="color: black">{{$publish->description}}</p>
-                                    <span class="text" style="color: black">{{date('d/m/Y', strtotime($publish->date_closure))}}</span>
-                                    <span>
+                                    <p class="text" style="color: black"> {{$publish->description}}</p>
+                                    <span class="text" style="color: black">Encerramento de Inscrições: {{date('d/m/Y', strtotime($publish->date_closure))}}</span>
                                     @endif
                                 </a>
-
                             @empty
                                 <b>Não há editais cadastrados até o momento!</b>
                             @endforelse
@@ -81,4 +72,5 @@
             </div>
         </div>
     </section>
+
 @stop
