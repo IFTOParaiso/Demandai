9cb3f20f535db026b778adb4af422884e547d9c4
romulo <romulo.rmos@gmail.com>
2018-12-10 19:01:01 -0300

    Alteração Seed TypeUser

--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -18,7 +18,16 @@ class UserValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'name' => 'required|min:3|max:100',
+            'email'=> 'email|unique:users,email',
+            //'password'=> 'alpha_num|between:6,20|Confirmed',
+            'password' => 'alpha_num|between:6,20',
+            'formation'=> 'required|cp|unique:users,cpf',
+            'lattes' => 'required|link',
+        ],
+
+
         ValidatorInterface::RULE_UPDATE => [],
     ];
 }
