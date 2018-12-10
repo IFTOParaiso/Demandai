f24d7004078c0776bde4671e287f6c2585adf897
Marques de Souza Almeida <marquesde2013@gmail.com>
2018-12-10 20:49:26 -0300

    Function mail

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -107,7 +107,7 @@ class UsersController extends Controller
                     'message' => $e->getMessageBag()
                 ]);
             }
-            mail('gerversonsa@gmail.com','testandooo','esse email e meramente teste porfavor nao exclua XD');
+            //mail('gerversonsa@gmail.com','testandooo','esse email e meramente teste porfavor nao exclua XD');
             return redirect()->back()->withErrors($e->getMessageBag())->withInput();
         }
     }
