89d395764cb82248d6d98cc4f9332695deb36098
romulo <romulo.rmos@gmail.com>
2018-12-10 21:22:40 -0300

    Alteração Tamanho Campo Descrição Edital

--- app/Validators/PublishValidator.php
+++ app/Validators/PublishValidator.php
@@ -23,6 +23,7 @@ class PublishValidator extends LaravelValidator
             'description' => 'required|min:5|max:1000',
             'date_closure' => 'required|date',
             'link' => 'required|url|min:10|max:255'
+
         ],
         ValidatorInterface::RULE_UPDATE => [
            'title' => 'required|min:5|max:100',
