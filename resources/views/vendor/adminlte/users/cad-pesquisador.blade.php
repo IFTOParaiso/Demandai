09ebacce9b36e52c0eae6e2e995fcf20362e3386
alvaro <alvarof.alves@hotmail.com>
2018-12-13 21:40:24 -0300

    register user

--- resources/views/vendor/adminlte/users/cad-pesquisador.blade.php
+++ resources/views/vendor/adminlte/users/cad-pesquisador.blade.php
@@ -41,7 +41,6 @@
                                             <div class="container-fluid">
 
                                                 <input type="hidden" name="status" value="1">
-
                                                 <div class="form-group has-feedback {{ $errors->has('name') ? 'has-error' : '' }}">
                                                     <input type="text" name="name" class="form-control"
                                                            value="{{ old('name') }}"
