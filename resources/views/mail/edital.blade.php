e778e8d6cbfc222aaf7597a6ef7b4d4d91d309f6
marquessouza <marquesde2013@gmail.com>
2018-12-17 21:29:46 -0300

    Email de Confirmação de Edital

---
+++ resources/views/mail/edital.blade.php
@@ -0,0 +1,10 @@
+<p style="color:#333">
+        Olá {{$nome}},<br>
+        O Edital {{$title}}<br>
+        Falando sobre:<br>
+        {{$descricao}}<br>
+        Mais informações no Link:{{$link}}<br>
+        {{$data}}
+
+
+</p>
