0fed8765e655106f8c31454dbf7fb389b5b1ae4a
alvaro <alvarof.alves@hotmail.com>
2019-01-21 16:47:39 -0300

    disable link tabs register publish

--- resources/views/vendor/adminlte/publishes/cad-publishes.blade.php
+++ resources/views/vendor/adminlte/publishes/cad-publishes.blade.php
@@ -9,9 +9,9 @@
     <div class="container-fluid">
         <div class="nav-tabs-custom">
             <ul class="nav nav-tabs" id="myTab">
-                <li class="active"><a href="#tab_1" data-toggle="tab">Edital</a></li>
-                <li><a href="#tab_2" data-toggle="tab">Áreas de Interesse</a></li>
-                <li><a href="#tab_3" data-toggle="tab">Lista de Trasmissão</a></li>
+                <li class="active"><a href="#tab_1" data-toggle="tab" style="pointer-events: none">Edital</a></li>
+                <li><a href="#tab_2" data-toggle="tab" style="pointer-events: none">Áreas de Interesse</a></li>
+                <li><a href="#tab_3" data-toggle="tab" style="pointer-events: none">Lista de Trasmissão</a></li>
                 <li class="pull-right header"><i class="fa fa-file"></i> Cadastro de Edital</li>
             </ul>
 
@@ -26,7 +26,7 @@
                                             <input type="text" name="title" id="title" class="form-control"
                                                    value="{{ old('title') }}"
                                                    placeholder="Título">
-                                            <strong id="error-title" style="color: red"></strong>
+                                            <strong id="error-title" style="color: #dd4b39"></strong>
                                             <span class="fa fa-file form-control-feedback"></span>
                                             @if ($errors->has('title'))
                                                 <span class="help-block">
@@ -37,7 +37,7 @@
                                         <div class="form-group has-feedback {{ $errors->has('description') ? 'has-error' : '' }}">
                         <textarea type="text" name="description" id="description" class="form-control" value="{{ old('description') }}"
                                   placeholder="Descrição" style="height: 100px" ></textarea>
-                                            <strong id="error-description" style="color: red"></strong>
+                                            <strong id="error-description" style="color: #dd4b39"></strong>
                                             <span class="fa fa-file-text form-control-feedback"></span>
                                             @if ($errors->has('description'))
                                                 <span class="help-block">
@@ -49,7 +49,7 @@
                                             <input type="text" name="date_closure" id="date_closure" class="form-control textbox-n"
                                                    placeholder="Data de Encerramento" onfocus="(this.type='date')"
                                                     onblur="compare();">
-                                            <strong id="error-date_closure" style="color: red"></strong>
+                                            <strong id="error-date_closure" style="color: #dd4b39"></strong>
                                             <div id="msdata"></div>
                                             <span class="fa fa-calendar form-control-feedback"></span>
                                             @if ($errors->has('date_closure'))
@@ -61,7 +61,7 @@
                                         <div class="form-group has-feedback {{ $errors->has('link') ? 'has-error' : '' }}">
                                             <input type="text" name="link" id="link" class="form-control"
                                                    placeholder="Link de Acesso" >
-                                            <strong id="error-link" style="color: red"></strong>
+                                            <strong id="error-link" style="color: #dd4b39"></strong>
                                             <span class="fa fa-link form-control-feedback"></span>
                                             @if ($errors->has('link'))
                                                 <span class="help-block">
