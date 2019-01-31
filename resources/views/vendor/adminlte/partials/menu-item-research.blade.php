e41f15419bbb79de759f2dd5428ad86a49460e08
alvaro <alvarof.alves@hotmail.com>
2019-01-31 00:02:31 -0300

    menu

---
+++ resources/views/vendor/adminlte/partials/menu-item-research.blade.php
@@ -0,0 +1,39 @@
+<section class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu" data-widget="tree">
+
+        <li id="editais">
+            <a href="http://localhost/listar-edital"
+            >
+                <i class="fa fa-fw fa-file "></i>
+                <span>Editais</span>
+            </a>
+        </li>
+        <li id="pesquisadores">
+            <a href="http://localhost/listar-usuario/pesquisador"
+            >
+                <i class="fa fa-fw fa-users "></i>
+                <span>Pesquisadores</span>
+            </a>
+        </li>
+        <li id="instituicao">
+            <a href="http://localhost/list-instituicoes"
+            >
+                <i class="fa fa-fw fa-university "></i>
+                <span>Instituições</span>
+            </a>
+        </li>
+        <li id="areas-pesquisa">
+            <a href="http://localhost/areas-pesquisa"
+            >
+                <i class="fa fa-fw fa-search "></i>
+                <span>Áreas de pesquisa</span>
+            </a>
+        </li>
+
+
+    </ul>
+    <!-- /.sidebar-menu -->
+</section>
+<!-- /.sidebar -->
