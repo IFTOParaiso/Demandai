84b71fed31067f8cc6531c9194ae43721b0014eb
Gerverson <gerversonsa@gmail.com>
2019-02-01 14:44:39 -0300

    Retirar edital com e-mail(nova mensagem)

--- resources/views/mail/ret_edital.blade.php
+++ resources/views/mail/ret_edital.blade.php
@@ -5,9 +5,9 @@
     {{--{{$descricao}}<br>--}}
     {{--Mais informações no Link:{{$link}}<br>--}}
     {{--{{$data}}--}}
-    Olá {{$nome}} temos uma notícia para você sobre o edital que foi retificado,que pode ser do seu interesse.<br>
-    <b><i>{{$title}}</i></b><br>
-    {{$descricao}}<br><br>
+    Olá {{$nome}}, o edital "<b>{{$title}}</b>" foi retificado.<br>
+    {{--<b><i>{{$title}}</i></b><br>--}}
+    Descrição do edital: {{$descricao}}<br><br>
     Só lembrando que encerra as inscrições dia {{date('d/M/y', strtotime($data))}}.<br>
     Mais informações você encontra no link:{{$link}}<br>
 </p>
